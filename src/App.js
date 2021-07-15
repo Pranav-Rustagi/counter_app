0cbc8370e79a039c8cfa3c3b25647c14feb3505c
Pranav-Rustagi <pranav001100@gmail.com>
2021-07-15 17:05:35 +0530

    completed counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+
+  let [count, setCount] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body">
+      <h1 className="h1">Simple Counter App</h1>
+
+      <div className="count">{count}</div>
+
+      <div className="btnCont">
+        <button className="btn" onClick={ () => checkCount(count - 1) ? setCount(count - 1) : null }>Decrement</button>
+        <button className="btn" onClick={ () => setCount(0) }>Reset</button>
+        <button className="btn" onClick={ () => checkCount(count + 1) ? setCount(count + 1) : null }>Increment</button>
+      </div>
     </div>
   );
 }
+
+const checkCount = count => 0 <= count && count <= 25;
 
 export default App;
